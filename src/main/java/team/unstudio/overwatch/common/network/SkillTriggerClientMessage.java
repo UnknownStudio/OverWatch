187557a3a08f4b87c261a6e3d538867b96ece582
Mouse <937279156@qq.com>
2017-02-01 22:35:22 +0800

    Fixed a bug

--- src/main/java/team/unstudio/overwatch/common/network/SkillTriggerClientMessage.java
+++ src/main/java/team/unstudio/overwatch/common/network/SkillTriggerClientMessage.java
@@ -3,16 +3,13 @@ package team.unstudio.overwatch.common.network;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.MinecraftForge;
-import team.unstudio.overwatch.event.SkillTiggerEvent;
 import team.unstudio.overwatch.hero.AbstractHero;
 
-import static team.unstudio.overwatch.hero.HeroManager.getCooldownTime;
 import static team.unstudio.overwatch.hero.HeroManager.getHero;
-import static team.unstudio.overwatch.hero.HeroManager.setCooldownTime;
 
 /**
  * Created by Mouse on 2017/1/27.
@@ -40,13 +37,13 @@ public class SkillTriggerClientMessage implements IMessage {
     public static final class Handler implements IMessageHandler<SkillTriggerClientMessage, IMessage> {
 
         @Override
+        @SideOnly(Side.CLIENT)
         public IMessage onMessage(SkillTriggerClientMessage message, MessageContext ctx) {
-            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+            EntityPlayer player = net.minecraft.client.Minecraft.getMinecraft().thePlayer;
             AbstractHero hero = getHero(player);
-            if (hero==null) return null;
-            hero.playerTriggerInClient(message.skill,player);
+            if (hero == null) return null;
+            hero.playerTriggerInClient(message.skill, player);
             return null;
         }
     }
-
 }
